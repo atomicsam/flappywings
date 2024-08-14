a8d56399ebb90aa5c322578c0ca6fbf819843675
Sam Saju <61165057+atomicsam@users.noreply.github.com>
2024-08-14 12:24:34 +0100

    Added pipe image

--- pipe.lua
+++ pipe.lua
@@ -4,11 +4,18 @@ function Pipe:new(x, y, maxHeight)
 	Pipe.super.new(self, x, y)
 
 	self.maxHeight = maxHeight
-	self.width = 70
 	self.height = love.math.random(50, maxHeight-200)
 	self.spacing = 175
 	self.speed = 150
+
+	self.pipe2y = self.height + self.spacing
+	self.pipe2height = self.maxHeight-self.height-self.spacing
+	
 	self.image = love.graphics.newImage("assets/Tiles/PipeStyle1.png")
+	self:importImage()
+
+	-- account for the quad and scale factor
+	self.width = (self.image:getWidth()/4)*3
 
 	self.passedFront = false
 	self.playerPassed = false
@@ -18,13 +25,20 @@ end
 
 function Pipe:draw()	
 	-- draw the top and bottom part of the pipe and create a gap for player to pass through
-	love.graphics.setColor(221/255, 119/255, 119/255)
+	love.graphics.setColor(119/255, 221/255, 119/255)
 	love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-	love.graphics.setColor(221/255, 119/255, 119/255)
-	love.graphics.rectangle("fill", self.x, self.height+self.spacing,
-							self.width, self.maxHeight-self.height-self.spacing)
 	love.graphics.setColor(1, 1, 1, 1)
-	love.graphics.draw(self.image, 0, 0)
+	love.graphics.draw(self.image, self.pipeStyles[self.currentStyle+1], self.x, 
+		self.height - self.frameHeight - 40, 0, 3, 3)
+	love.graphics.draw(self.image, self.pipeExtension[1], self.x, 0, 0, 3, self.height-self.frameHeight)
+
+	-- draw bottom pipe
+	love.graphics.setColor(119/255, 221/255, 119/255)
+	love.graphics.rectangle("fill", self.x, self.pipe2y,
+							self.width, self.pipe2height)
+	love.graphics.setColor(1, 1, 1, 1)
+	love.graphics.draw(self.image, self.pipeStyles[self.currentStyle], self.x, self.pipe2y, 0, 3, 3)
+	love.graphics.draw(self.image, self.pipeExtension[1], self.x, self.pipe2y+(self.image:getHeight()/4)*3, 0, 3, 3*self.pipe2height)
 end
 
 function Pipe:update(dt)
@@ -37,7 +51,7 @@ function Pipe:reachedEnd()
 end
 
 function Pipe:passedEnd()
-	return self.x <= 0 - self.width
+	return self.x <= 0 - self.width - 20
 end
 
 function Pipe:updateScore(player)
@@ -67,12 +81,25 @@ end
 
 function Pipe:importImage()
 	self.pipeStyles = {}
-	self.rows = 2
-	self.colums = 4
+	self.currentStyle = 1
 
-	for i=1,4 do
-		for j=1,2 do
-			table.insert(self.pipeStyles, love.graphics.newQuad(i*self.image:getHeight()/2))
+	local rows = 2
+	local columns = 4
+	local frameWidth = self.image:getWidth()/columns
+	self.frameHeight = self.image:getHeight()/rows
+
+	for i=0,1 do
+		for j=0,3 do
+			for k=0,1 do
+				table.insert(self.pipeStyles, love.graphics.newQuad(j*frameWidth,
+								i*self.frameHeight+(k*self.frameHeight*0.5),
+								frameWidth, self.frameHeight/2, self.image:getWidth(),
+								self.image:getHeight()))
+			end
 		end
 	end
+
+	self.pipeExtension = {}
+	table.insert(self.pipeExtension, love.graphics.newQuad(0, 30, frameWidth, 1, self.image:getWidth(),
+		self.image:getWidth()))
 end
